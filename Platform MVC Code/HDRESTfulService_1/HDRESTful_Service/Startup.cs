a92de2d06fed7ea2a86d7899c8a708dbbaaa212e
blazingsoldier08 <contagious.thoughts@gmail.com>
2015-02-27 19:50:31 -0500

    ServerPlatformCode
    The initial code for the platform

---
+++ Platform MVC Code/HDRESTfulService_1/HDRESTful_Service/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HDRESTful_Service.Startup))]
+namespace HDRESTful_Service
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
